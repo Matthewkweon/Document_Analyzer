2b830a56744b89422dd107fd794f8ffb8ad5c044
Yongjoon Kweon <Mattquan10@gmail.com>
2025-02-13 18:29:04 -0500

    Create document.py

---
+++ document.py
@@ -0,0 +1,5 @@
+# I want to upload material for students to interact with
+# I want to create quizes for students to check their progress(LLM)
+# I want the system to grade the students
+# I want the students to comment on material
+# I want the students to get their own progress
